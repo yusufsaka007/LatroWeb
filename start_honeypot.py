f080ee91c9552d32f561e12e7c9b9c1a91efe1b5
viv4ldi <viv4ldi@ARCH4EA.arch4ea.net>
2025-03-24 01:26:50 -0500

    Updated python. Able to create the virtual environment

--- start_honeypot.py
+++ start_honeypot.py
@@ -7,16 +7,9 @@ from colorama import Fore, Back, Style, init
 
 init(autoreset=True)
 
-'''
-print(Fore.RED + "This is red text")
-print(Fore.GREEN + "This is green text")
-print(Back.YELLOW + "This has a yellow background")
-print(Style.BRIGHT + "This is bright text")
-'''
-
 ROOT_FOLDER = "./honeypot"
-FOLDER_STRUCTURE = ['bin','lib/x86_64-linux-gnu','lib64','usr/bin','usr/sbin','etc','home','var/www/html']
-COMMAND_LIST = ['ls','cat','bash','passwd','useradd','ps','whoami','hostname']
+FOLDER_LIST = ['lib/x86_64-linux-gnu','lib64','usr/bin','usr/sbin','etc','home','var/www/html']
+COMMAND_LIST = ['ls','cat','bash','passwd','useradd','whoami','hostname']
 LIB_LIST = []
 
 def run_command(command, args):
@@ -36,8 +29,12 @@ def create_folder(folder):
     # check if parent folder exists
 
 # Create the virtual folder under honeypot/ which is exactly the scommand
+def default_structure():
+    for folder in FOLDER_LIST:
+        create_folder(os.path.join(ROOT_FOLDER, folder))
 # Copy the libraries the copied commands need
 def copy_libraries(command):
+    print(Style.BRIGHT + f"\nCopying libraries for {command}\n")
     command_path = run_command('which', [command])
     out = run_command('ldd', [f'{command_path.strip()}'])
     if out:
@@ -45,53 +42,106 @@ def copy_libraries(command):
         for line in out_split:
             if '/' in line:
                 if '=>' in line:
-                    lib_path = line.split('=>')[1].strip()
+                    lib_path = line.split('=>')[1].strip().split('(')[0].strip()
                 else:
                     lib_path = line.split('(')[0].strip()
-                print(Back.YELLOW + lib_path)
+                target_path = os.path.join(ROOT_FOLDER, lib_path[1:].strip())
+                if not os.path.exists(target_path):
+                    print(Back.BLUE + f"Copying {lib_path}  ===>  {target_path}")
+                    run_command('cp', [lib_path, target_path])
+                else:
+                    print(Back.YELLOW + f"[!] {lib_path} already copied to {target_path}")
 # Copy the commands which will be available in the virtual environment
 def copy_commands():
     for command in COMMAND_LIST:
         try:
-            command_path = run_command('which', [command])
+            command_path = run_command('which', [command]).strip()
             if command_path:
-                target_path = os.path.join(ROOT_FOLDER, command_path[1:].strip())
-                if not os.path.exists(command_path):
-                    out = run_command('cp', [command_path.strip(), target_path])
-                    if out:
-                        #copy_libraries(command)
-                        print(Fore.GREEN + f"[+] Copied {command}")
-                        pass
+                if not os.path.exists(os.path.join(ROOT_FOLDER, os.path.dirname(command_path[1:]))):
+                    create_folder(os.path.join(ROOT_FOLDER, os.path.dirname(command_path[1:])))
+                target_path = os.path.join(ROOT_FOLDER, command_path[1:]).strip()
+                if not os.path.exists(target_path):
+                    run_command('cp', [command_path, target_path])
+                    print(Fore.GREEN + f"[+] Copied {command}")
+                    copy_libraries(command) # Copying the libraries for the command
                 else:
-                    print(Fore.YELLOW + f"[!] {target_path} already copied")
+                    print(Fore.YELLOW + f"[!] {command_path} already copied to {target_path}")
         except Exception as e:
             print(Fore.RED + f"[-] e")
 
-# Create a user 'yusuf' with password 'password' using chroot
+# Create a user and set the password. Define the /etc/passwd and /etc/shadow files
 
-# Make everything the libraries read only
+# Make everything read only. Avoid writing to files especially libraries
 
-# Final security measures
+# Final security measures. Make sure attacker can't get out laterally
 
 # Run commands using chroot (set honeypot as root folder)
 
+# Custom command/folder handle
+def custom_folders(cf_arg):
+    if len(cf_arg) == 1:
+        if os.path.exists(cf_arg[0]) and os.path.isfile(cf_arg[0]):
+            with open(cf_arg[0], 'r') as f:
+                for line in f.readlines():
+                    create_folder(os.path.join(ROOT_FOLDER, line.strip()))
+        elif not "." in cf_arg[0]:
+            create_folder(os.path.join(ROOT_FOLDER, cf_arg[0]))
+        else:
+            print(Fore.RED + "[-] Invalid syntax")
+    else:
+        for folder in cf_arg:
+            if not "." in folder:
+                create_folder(os.path.join(ROOT_FOLDER, folder))
+            else:
+                print(Fore.RED + "[-] Invalid syntax")
 
+def redefine_commands(cc_arg):
+    global COMMAND_LIST
+    if len(cc_arg) == 1 and ".txt" in cc_arg[0]:
+        arg = cc_arg[0]
+        if os.path.exists(arg) and os.path.isfile(arg):
+            with open(arg, 'r') as f:
+                COMMAND_LIST = [command_path.strip() for command_path in f.readlines()]
+        else:
+            print(Fore.RED + "[-] Command list file does not exist")
+    else:
+        COMMAND_LIST = cc_arg
+    
 def main():
     parser = argparse.ArgumentParser(description="Start a honeypot with the given username and password")
-    parser.add_argument("--username", "-u", help="Username of the virtual environment", default="guest")
-    parser.add_argument("--password", "-p", help="Password of the virtual environment", default="password")
-    parser.add_argument("--hostname", "-hn", help="Computer name/hostname of the virtual environment", default="honeypot")
+    parser.add_argument("--username", "-u", help="Username of the virtual environment")
+    parser.add_argument("--password", "-p", help="Password of the virtual environment")
+    parser.add_argument("--hostname", "-hn", help="Computer name/hostname of the virtual environment")
+    parser.add_argument("--custom-folder", "-cf", help="Custom folder to create on top of the ./honeypot. -cf <folder_path> | -cf <folder_path1 folder_path2...> | -cf <folder_list.txt> | e.g -cf bin/new_virtual_folder", nargs="+", required=False)
+    parser.add_argument("--custom-command", "-cc", help="Custom command to add to the virtual environment. Custom command list for this flag requires to be in .txt format. Requires the absolute path | -cc <path_to_command> | -cc <path_to_command1 path_to_command2...> | -cc <commands_list.txt> | e.g -cc /bin/bash ", nargs="+", required=False)
+    parser.add_argument("--virtual", "-v", help="Enable virtual environment setup (copy commands, libraries, etc.)", action="store_true", default=False)
+
     args = parser.parse_args()
 
     # Check root priviliges
     if os.getuid() != 0:
         print(Fore.RED + "Please run this script as root")
         sys.exit(1)
-    
-    #run_command('echo', [f"{args.username}-{args.password}"])
-    #create_virtual_structure()
-    #copy_commands()
+    if args.virtual:
+        print(Fore.GREEN + "[+] Setting up the virtual environment")
+        if args.custom_folder:
+            custom_folders(args.custom_folder)
+        else:
+            default_structure()
 
-    copy_libraries("passwd")
+        if args.custom_command:
+            redefine_commands(args.custom_command) # Change the default COMMAND_LIST
+        copy_commands()
+    else:
+        if args.username:
+            if args.password:
+                pass
+            else:
+                pass
+        if args.hostname:
+            pass
+        else:
+            print(Fore.RED + "[-] Invalid syntax. Try -h for help")
+
 if __name__ == '__main__':
     main()
